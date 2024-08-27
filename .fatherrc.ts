844eba3d5e00f48978a1689f7fcbe46387568c81
wangxiangsong <951606897@qq.com>
2024-08-27 14:56:30 +0800

    feat: 配置修改

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -5,15 +5,4 @@ export default defineConfig({
   esm: {
     output: 'dist', // 输出目录，存放 ES Module 格式的代码
   },
-
-  extraBabelPlugins: [
-    [
-      'import',
-      {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true, // 如果需要引入 less 文件可以设置为 true
-      },
-    ],
-  ],
 });
