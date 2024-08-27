f484ddc383320e3fb38f54d0d373c4621e83d545
wangxiangsong <951606897@qq.com>
2024-08-27 13:49:52 +0800

    Updates

--- Button/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { type FC } from 'react';
-declare const Button: FC<{
-    title: string;
-}>;
-export default Button;
--- Button/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-var Button = function Button(props) {
-  return /*#__PURE__*/React.createElement("h1", null, props.title);
-};
-export default Button;
